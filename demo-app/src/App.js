5be67fbbfbde2a33ce7cf8d958d0ab23837ac41b
kavitamakariye <kavitasmakariye@gmail.com>
2021-01-10 12:35:46 +0530

    implemented routing using react-router-dom library

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'; 
+import Home from './component/home'; 
+import About from './component/about'; 
+import Contact from './component/contact';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+     /* React Router is a standard library for routing in React. 
+     Command to install npm install react-router-dom –save */
+    return (
+      /* BrowserRouter is a router implementation that uses the HTML5 history API.
+         Here it is aliased as Router in import. */
+      <Router> 
+        <div className="App">
+          <ul>
+            <li>
+              {/* Link component is used to create links to different routes and
+               implement navigation around the application.
+               It works like HTML anchor tag. */}
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          {/* Switch component is used to render only the first route that matches the location rather than rendering all matching routes. */}
+          <Switch> 
+            {/* Route is the conditionally shown component that renders some UI when its path matches the current URL */}
+              {/* exact is used to match the exact value with the URL. */}
+              <Route exact path='/' component={Home}></Route> 
+              <Route exact path='/about' component={About}></Route> 
+              <Route exact path='/contact' component={Contact}></Route> 
+            </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
